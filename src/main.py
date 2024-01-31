cda5dc5d70cf0f36f0a8939cbb2e3af735f86c6d
gowthaml15 <gowtham.mlresearcher7@gmail.com>
2024-01-31 22:01:15 +0530

    Updated main to langchain

--- src/main.py
+++ src/main.py
@@ -1,69 +1,69 @@
 from utils.imports import *
+import streamlit as st
+from streamlit_chat import message
 
 
-load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv('OpenAI-API-Key')
+load_dotenv("src/.env")
+os.environ["OPENAI_API_KEY"] = os.getenv("OpenAI-API-Key")
 
 
-def get_response_llm(file, question):
+# Initialize session state variables
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+
+
+
+# Sidebar - user can choose model and upload a CSV file
+st.sidebar.title("Sidebar")
+
+# Update this section
+uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+
+# CSV handling logic - Update this section
+if uploaded_file is not None:
+    print(uploaded_file)
+    df = pd.read_csv(uploaded_file)
+    # You can use the DataFrame 'df' in your chat logic or display it as needed
+    st.table(df.head())
+    # Update this section with your desired save path
+    save_path = "src/uploads/data.csv"
+    st.sidebar.write(f"CSV file saved at: {save_path}")
+
+    # Save the DataFrame to CSV
+    df.to_csv(save_path, index=False)
+
+
+# Generate a response function
+def generate_response(prompt):
+    # Generate response
     agent = create_csv_agent(
-    OpenAI(temperature=0),
-    file,
-    verbose=True,
-    agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-)
-    return agent.run(question)
+        OpenAI(temperature=0),
+        os.path.join(os.getcwd(),"src/uploads/data.csv"),
+        # verbose=True,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    )
+    return agent.run(prompt)
 
-# Create a placeholder for chat messages
-# Create a placeholder for chat messages
-chat_messages = []
+# Container for chat history
+response_container = st.container()
+# Container for text box
+container = st.container()
 
-def main():
-    st.title("Talk with your CSV")
+with container:
+    with st.form(key='my_form', clear_on_submit=True):
+        user_input = st.text_area("You:", key='input', height=100)
+        submit_button = st.form_submit_button(label='Send')
 
-    # Sidebar for file upload
-    uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
-
-    # If a file is uploaded
-    if uploaded_file is not None:
-        # Read the CSV file into a DataFrame
-        df = pd.read_csv(uploaded_file)
-
-        # Display the top 5 rows in a compact table
-        st.table(df.head())
-
-        # Show messages in the chat
-        show_chat()
-
-        # Generate a unique filename with a timestamp
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        unique_filename = f"uploaded_file_{timestamp}.csv"
-        save_path = os.path.join("uploads",unique_filename)
-        df.to_csv(save_path, index=False)
-
-        # Transition to chat mode
-        st.header("Chat Mode")
-
-    # Chat input
-    user_input = st.text_area("You:", "", key="user_input")
-
-    # Chat response
-    if user_input:
-        chat_messages.append(f"**You:** {user_input}")
-        # Add your logic here to generate a chatbot response or any other interactions
-        chat_messages.append(f"**Analyst:** {get_response_llm(save_path,user_input)}")
-
-    # Show messages in the chat
-    show_chat()
-
-def show_chat():
-    # Display chat messages with chat-like styling
-    for message in chat_messages:
-        if "**You:**" in message:
-            st.markdown(f'<div style="text-align: right; color: #4184f3; padding: 5px;">{message}</div>', unsafe_allow_html=True)
-        else:
-            st.markdown(f'<div style="text-align: left; color: #4a4a4a; padding: 5px;">{message}</div>', unsafe_allow_html=True)
+    if submit_button and user_input:
+        output = generate_response(user_input)
+        st.session_state['past'].append(user_input)
+        st.session_state['generated'].append(output)
 
 
-if __name__ == "__main__":
-    main()
+if st.session_state['generated']:
+    with response_container:
+        for i in range(len(st.session_state['generated'])):
+            message(st.session_state["past"][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["generated"][i], key=str(i))
